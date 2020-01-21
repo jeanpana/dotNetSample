139a0991e4eeec896d9bf623d92b71a3c1983636
jeanpana <39168471+jeanpana@users.noreply.github.com>
2020-01-21 08:38:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Demo
+{
+    class Car
+    {
+        int Plate;
+        string Model;
+
+        public void SetCar(int carno, string model)
+        {
+            Plate = carno;
+            Model = model;
+        }
+        public String GetCar()
+        {
+            return Model;
+        }
+
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.SetCar(1234, "Honda");
+
+            Console.WriteLine(car.GetCar());
+        }
+    }
+}
